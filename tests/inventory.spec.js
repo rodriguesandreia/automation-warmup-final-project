24bda0f8286c6b70620de1934a8f2b60b0bba273
Andreia Rodrigues <andreia.rodrigues@mindera.com>
2025-11-20 22:50:08 +0000

    increase and decrease

--- tests/inventory.spec.js
+++ tests/inventory.spec.js
@@ -1,20 +1,20 @@
 import { test, expect } from "@playwright/test";
+import { products } from "./data/inventory.data";
 
 test("Add product", async ({ page }) => {
-  await page.goto("https://playground-drab-six.vercel.app/");
-
-  // Expect a title "to contain" a substring.
-
-  await expect(page).toHaveTitle("Playground page");
-  await page.getByRole("link", { name: "STORE" }).click();
+  await page.goto("https://playground-drab-six.vercel.app/store");
   await expect(page.getByTestId("instructions-title")).toBeVisible();
   await page.getByTestId("store-tab-inventory").click();
   await expect(page.getByTestId("inventory-title")).toBeVisible();
 
   await test.step("Fill in", async () => {
-    await page.getByTestId("inventory-input-name").fill("Bananas (Minions)");
-    await page.getByTestId("inventory-input-price").fill("2.50");
-    await page.getByTestId("inventory-input-quantity").fill("10");
+    await page.getByTestId("inventory-input-name").fill(products.bananas.name);
+    await page
+      .getByTestId("inventory-input-price")
+      .fill(products.bananas.price);
+    await page
+      .getByTestId("inventory-input-quantity")
+      .fill(products.bananas.quantity);
   });
 
   await test.step("Click button", async () => {
@@ -23,13 +23,43 @@ test("Add product", async ({ page }) => {
 
   await test.step("Verify product", async () => {
     await expect(page.getByTestId("inventory-product-name-8")).toHaveText(
-      "Bananas (Minions)"
+      products.bananas.name
     );
     await expect(
       page.getByTestId("inventory-product-price-value-8")
-    ).toHaveText("2.50");
+    ).toHaveText(products.bananas.price);
     await expect(page.getByTestId("inventory-product-quantity-8")).toHaveText(
-      "10"
+      products.bananas.quantity
     );
   });
+});
+
+test("Increase quantity", async ({ page }) => {
+  await page.goto("https://playground-drab-six.vercel.app/store");
+  await expect(page.getByTestId("instructions-title")).toBeVisible();
+  await page.getByTestId("store-tab-inventory").click();
+  await expect(page.getByTestId("inventory-title")).toBeVisible();
+
+  if (page.getByTestId("inventory-product-0")) {
+    const iniQuant = await page.getByTestId("inventory-product-quantity-0").innerText();
+    console.log(iniQuant);
+    await page.getByTestId("inventory-product-increase-0").click();
+    const finQuant = await page.getByTestId("inventory-product-quantity-0").innerText();
+    expect (Number(finQuant)).toBeGreaterThan(Number(iniQuant));
+  }
+});
+
+test("Decrease quantity", async ({ page }) => {
+  await page.goto("https://playground-drab-six.vercel.app/store");
+  await expect(page.getByTestId("instructions-title")).toBeVisible();
+  await page.getByTestId("store-tab-inventory").click();
+  await expect(page.getByTestId("inventory-title")).toBeVisible();
+
+  if (page.getByTestId("inventory-product-0")) {
+    const iniQuant = await page.getByTestId("inventory-product-quantity-0").innerText();
+    console.log(iniQuant);
+    await page.getByTestId("inventory-product-decrease-0").click();
+    const finQuant = await page.getByTestId("inventory-product-quantity-0").innerText();
+    expect (Number(finQuant)).toBeLessThan(Number(iniQuant));
+  }
 });
