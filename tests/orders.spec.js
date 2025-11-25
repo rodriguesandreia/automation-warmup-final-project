68211451f218e3f91f57f226d750c8c01487e5d3
Andreia Rodrigues <andreia.rodrigues@mindera.com>
2025-11-25 14:46:40 +0000

    missing validations on the order test

--- tests/orders.spec.js
+++ tests/orders.spec.js
@@ -2,10 +2,27 @@ import { test, expect } from "@playwright/test";
 import { makeOrder, getOrderItemInfo } from "./utils/helpers.js";
 
 test("Validate order details", async ({ page }) => {
-  const { productName, productPrice } =
+  const { productName, productPrice, orderDate, paymentMethod } =
     await test.step("Make an order", async () => {
       return await makeOrder(page);
     });
+
+    await test.step("Validate date", async () => {
+    expect(await page.getByTestId('order-date-0').innerText()).toBe(`Date: ${orderDate}`);
+  });
+
+  await test.step("Validate payment method", async () => {
+    expect(await page.getByTestId("order-payment-0").innerText()).toBe(
+      `Payment Method: ${paymentMethod}`
+    );
+  });
+
+  await test.step("Validate product name & quantity", async () => {
+    const { orderQuantity, orderName } = await getOrderItemInfo(page, 0, 0);
+    expect(orderName).toBe(productName);
+    //quantity is 1 as only 1 item was added to the cart
+    expect(orderQuantity).toBe(1);
+  });
 
   await test.step("Validate product price", async () => {
     const price = Number(
@@ -28,12 +45,5 @@ test("Validate order details", async ({ page }) => {
       await page.getByTestId("order-total-value-0").innerText()
     );
     expect(paymentTotal).toBe(total);
-  });
-
-  await test.step("Validate product name & quantity", async () => {
-    const { orderQuantity, orderName } = await getOrderItemInfo(page, 0, 0);
-    expect(orderName).toBe(productName);
-    //quantity is 1 as only 1 item was added to the cart
-    expect(orderQuantity).toBe(1);
   });
 });
--- tests/utils/helpers.js
+++ tests/utils/helpers.js
@@ -110,15 +110,24 @@ export async function makeOrder(page) {
 
   // select payment method
   await page.getByTestId("payment-method-input-MBWay").click();
-
+  const paymentMethod = await page
+    .getByTestId("payment-method-input-MBWay")
+    .inputValue();
   // click confirm button
   await page.getByTestId("payment-confirm-button").click();
+
+  const now = new Date();
+  // format date to match the order's screen
+  const orderDate =
+    now.toLocaleDateString("en-US") +
+    ", " +
+    now.toLocaleTimeString("en-US", { hour12: true });
 
   //check if the the Order page opens and the order is there
   await expect(page.getByTestId("orders-title")).toBeVisible();
   await expect(page.getByTestId("order-0")).toBeVisible();
 
-  return { productName, productPrice };
+  return { productName, productPrice, orderDate, paymentMethod };
 }
 
 export async function getOrderItemInfo(page, row = 0, col = 0) {
